313ab0b5ecdc6b7e4aa854fe396fce820011a132
John <jmasulonis@mac.com>
2021-04-22 20:57:52 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+const items = document.querySelectorAll("#timeline li");
+const scrollToTopButton = document.getElementById("js-top");
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add("show");
+    }
+  });
+
+const scrollFunc = () => {
+  let y = window.scrollY;
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+scrollToTopButton.onclick = function (e) {
+  e.preventDefault();
+  scrollToTop();
+};
+
+// Events
+window.addEventListener("scroll", scrollFunc);
+window.addEventListener("load", run);
+window.addEventListener("resize", run);
+window.addEventListener("scroll", run);
